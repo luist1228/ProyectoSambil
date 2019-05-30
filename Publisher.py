76b6f6a2477ad60dcc111db6b20bce739b32bc0a
unknown <luis.t1228@gmail.com>
2019-05-30 17:39:13 -0400

    Queries de Schema y conteo de camaras

--- Publisher.py
+++ Publisher.py
@@ -8,16 +8,26 @@ import datetime
 import pandas as pd
 import psycopg2 as psy
 
-conn=psy.connect(host = 'localhost', user= 'postgres', password ='12t09lma', dbname= 'Lab2')
+conn=psy.connect(host = 'localhost', user= 'postgres', password ='12t09lma', dbname= 'SambilDB')
+
+def contarCamaras():
+  #Numero de personas
+  sql='''SELECT * FROM public."camara" ;'''
+  df = pd.read_sql_query(sql, conn)
+  camaras=df.id.count()
+  return camaras
 
 def main():
-    client = paho.mqtt.client.Client(client_id='Camaras',clean_session=False)
-    client.qos = 0
-    client.connect(host='localhost')
-    payload = {
-        "Mensaje": "Holaaa"
-		}
-    client.publish('Sambil/Camaras',json.dumps(payload),qos=0)
+  client = paho.mqtt.client.Client(client_id='Camaras',clean_session=False)
+  client.qos = 0
+  client.connect(host='localhost')
+  cantidad=contarCamaras()
+  print(cantidad)
+    # while(cantidad>0)
+  payload = {
+    "Mensaje": "Holaaa"
+  }
+  client.publish('Sambil/Camaras',json.dumps(payload),qos=0)
 
 
 if __name__ == '__main__':
---
+++ queries Proyecto.sql
@@ -0,0 +1,87 @@
+create table Beacon(
+	id serial primary key,
+	modelo varchar(20)
+);
+
+
+create table Tienda(
+	id serial primary key,
+	nombre varchar(20),
+	dueño varchar(20),
+	horaA time,
+	horaC time,
+	fkBeacon int references Beacon(id) 
+);
+
+
+create table Entrada(
+	id serial primary key,
+	Abierto boolean
+);
+
+
+create table Mesa(
+	id serial primary key,
+	puestos int2,
+	fkBeacon int references Beacon(id)
+);
+
+
+create table Camara(
+	id serial primary key,
+	fkEntrada int references Entrada(id),
+	modelo varchar(20)
+);
+
+
+create table EntradaP(
+	id serial primary key,
+	fkCamara int references Camara(id),
+	sexo varchar(10),
+	edad int,
+	macAdd varchar(20),
+	registroE timestamp
+);
+
+
+create table SalidaP(
+	id serial primary key,
+	fkCamara int references Camara(id),
+	registroS timestamp,
+	macAdd int references EntradaP(id)
+);
+
+
+create table Persona(
+	id serial primary key,
+	macAddres varchar(20),
+	nombre varchar(20),
+	apellido varchar(20)
+);
+
+
+create table Compra (
+	id serial primary key,
+	fkTienda int references Tienda(id),
+	fkPersona int references Persona(id),
+	fecha timestamp,
+	total int
+);
+
+
+create table RegistroB(
+	id serial primary key,
+	macadd int references Persona(id),
+	tiempoI timestamp,
+	tiempoF timestamp
+);
+
+
+
+select count("id") from entradap as e
+where date_part('day',e."registroe")=date_part('day',now())
+
+select count("id") from salidap as e
+where date_part('day',e."registros")=date_part('day',now())
+
+
